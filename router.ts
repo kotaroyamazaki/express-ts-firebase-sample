b4a97684dfde089d445902af21d18672e9bf7321
KotaroYamazaki <sentoryoku53m@gmail.com>
2022-06-09 00:01:37 +0900

    fix

--- router.ts
+++ router.ts
@@ -6,8 +6,8 @@ passport.use(
   "mylogin",
   new LocalStrategy(
     {
-      usernameField: "username", // formのname属性が"username"を指定
-      passwordField: "password", // formのname属性が"password"を指定
+      usernameField: "username",
+      passwordField: "password",
     },
     (
       username: string,
@@ -17,10 +17,9 @@ passport.use(
       // ログイン処理本体
       // formから受け取ったusernameが"user"、passwordが"password"ならばログイン成功とする
       if (username === "user" && password === "password") {
-        return done(null, !!username); // ログイン成功と判定する
+        return done(null, !!username);
       }
-
-      return done(null, false); // ログイン失敗と判定する
+      return done(null, false);
     }
   )
 );
