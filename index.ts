db14d25cca9787fb5770aef4ef2240124e48de81
KotaroYamazaki <sentoryoku53m@gmail.com>
2022-06-12 11:11:08 +0900

    wip

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const morgan = require("morgan");
+const PORT = process.env.PORT || 3000;
 
+// middleware
 app.use(express.json());
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 
+// routes
 app.use("/", require("./router.ts"));
 
 app.listen(3000);
--- router.ts
+++ router.ts
@@ -1,82 +1,32 @@
 const router = require("express").Router();
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-// const firebase = require("firebase");
-// const firebaseui = require("firebaseui");
 
-// const { initializeApp } = require("firebase/app");
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAlzR2wCJ6bZ0IkoaOMwizgLMwJmVBMh2U",
-//   authDomain: "toytoy-box.firebaseapp.com",
-//   projectId: "toytoy-box",
-//   storageBucket: "toytoy-box.appspot.com",
-//   messagingSenderId: "772796561601",
-//   appId: "1:772796561601:web:8286a5a5fde8225aa9bc40",
-// };
+import { Request, Response } from "express";
 
-// const fb = initializeApp(firebaseConfig);
-
-// // Initialize the FirebaseUI Widget using Firebase.
-// const ui = new firebaseui.auth.AuthUI(firebase.auth());
-// const uiConfig = {
-//   callbacks: {
-//     // uiShown: function () {
-//     //   document.getElementById("loader").style.display = "none";
-//     // },
-//   },
-//   signInFlow: "popup",
-//   signInSuccessUrl: "/",
-//   signInOptions: [
-//     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-//     firebase.auth.EmailAuthProvider.PROVIDER_ID,
-//   ],
-// };
-
-passport.use(
-  "mylogin",
-  new LocalStrategy(
-    {
-      usernameField: "username",
-      passwordField: "password",
-    },
-    (
-      username: string,
-      password: string,
-      done: (arg0: null, arg1: boolean) => any
-    ) => {
-      if (username === "user" && password === "password") {
-        return done(null, !!username);
-      }
-      return done(null, false);
-    }
-  )
-);
-
-router.use(passport.initialize());
-
-router.get("/", (req: any, res: any) => {
-  res.send("Hello World!");
+router.get("/", (req: Request, res: Response) => {
+  res.send("hello world");
 });
 
-// router.get("/fb", (req: any, res: any) => {
-//   res.render(ui.start("#firebaseui-auth-container", uiConfig));
+router.get("/upload", (req: Request, res: Response) => {
+  res.render("./upload.ejs");
+});
+
+// import { getStorage, ref, uploadBytes } from "firebase/storage";
+
+// const storage = getStorage();
+// const storageRef = ref(storage, "express-sample");
+// const os = require("os");
+// const multer = require("multer");
+// const upload = multer({ dest: os.tmpdir() });
+
+// 'file' comes from the Blob or File API
+
+// router.post("/upload", (req: any, res: Response) => {
+//   console.log(req.file);
+
+//   uploadBytes(storageRef.bucket., req.file).then((snapshot) => {
+//     console.log("Uploaded a blob or file!");
+//   });
+//   res.send("uploaded");
 // });
-
-router.get("/login", (req: any, res: any) => {
-  res.render("./login.ejs");
-});
-
-router.post(
-  "/login",
-  passport.authenticate("mylogin", {
-    successRedirect: "/ok",
-    failureRedirect: "/login",
-    session: false,
-  })
-);
-
-router.get("/ok", (req: any, res: any) => {
-  res.render("./ok.ejs");
-});
 
 module.exports = router;
