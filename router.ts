c4654660eaf1840e287c55ccce11b6f4b881f592
KotaroYamazaki <sentoryoku53m@gmail.com>
2022-06-15 09:41:54 +0900

    sandbox for db-connection

--- router.ts
+++ router.ts
@@ -1,6 +1,15 @@
 import { Request, Response } from "express";
 import multer from "multer";
 import { uploadFS } from "./firebase";
+const pgp = require("pg-promise")(/* options */);
+const connection = {
+  host: "localhost",
+  port: 5432,
+  database: "tocre",
+  user: "postgres",
+  password: "",
+};
+const db = pgp(connection);
 const router = require("express").Router();
 
 router.get("/", (req: Request, res: Response) => {
@@ -21,6 +30,16 @@ router.post("/upload", multer().single("file"), (req: any, res: Response) => {
 
 router.get("/success", (req: Request, res: Response) => {
   res.send("upload success");
+});
+
+router.get("/users/1", (req: Request, res: Response) => {
+  db.one("SELECT * FROM users WHERE id=$1", [1])
+    .then((data: any) => {
+      res.send(data);
+    })
+    .catch((error: any) => {
+      console.log("ERROR:", error);
+    });
 });
 
 module.exports = router;
